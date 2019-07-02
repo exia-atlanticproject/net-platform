d7c0a9f72f664de50365e72f78f2c01f6b7ee341
Nicolas <nicolas.lespinasse@gmail.com>
2019-07-02 11:03:59 +0200

    Remove local db

--- config/exec.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ $1 = "test" ]; then
-	cd /home/
-	dotnet test
-else
-	cd /home/net-platform/
-	dotnet build
-	/usr/share/dotnet/dotnet /home/net-platform/bin/Debug/netcoreapp2.2/net-platform.dll
-fi
